0be8858c216da0311ba3e8fb2c19e3a3ad8c4b8d
John Kulas <jtkulas@ergreports.com>
2026-03-04 14:08:49 -0600

    student ppt

--- app.R
+++ app.R
@@ -3,7 +3,7 @@ library(shiny)
 library(DBI)
 library(RPostgres)
 
-db <- sd_db_connect(ignore = FALSE)  # Ignore database for testing
+db <- sd_db_connect(ignore = TRUE)  # Ignore database for testing
 
 server <- function(input, output, session) {
   sd_skip_if()
